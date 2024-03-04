80c039ab50ca235c85168165a44c409e6f7845ca
abaabaman <417026323@qq.com>
2024-03-04 13:27:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+// import { ElButton } from 'element-plus'
+// export default {
+//   components: { ElButton },
+// }
+</script>
+
+<template>
+  <Header />
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+main {
+  position: absolute;
+  width: 100%;
+  top: 4rem;
+}
+
+
+
+
+
+@media (min-width: 1024px) {}
+</style>
---
+++ src/views/GameView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+    这里是游戏
+</template>
